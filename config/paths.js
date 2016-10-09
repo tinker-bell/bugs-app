a202bb06c0f4878bfd70eabfc004e63fd9439ec5
tinker-bell <olga.kertling@gmail.com>
2016-10-09 23:28:21 +0300

    Build folder changed to build/www for app engine deployment

--- config/paths.js
+++ config/paths.js
@@ -26,7 +26,7 @@ var nodePaths = (process.env.NODE_PATH || '')
 
 // config after eject: we're in ./config/
 module.exports = {
-  appBuild: resolveApp('build'),
+  appBuild: resolveApp('build/www'),
   appHtml: resolveApp('index.html'),
   appIndexJs: resolveApp('src/index.js'),
   appPackageJson: resolveApp('package.json'),
